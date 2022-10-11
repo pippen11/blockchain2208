5b79da39081dbbe34acf6ee8c2ad61007af0c273
pippen11 <111038332+pippen11@users.noreply.github.com>
2022-10-11 18:49:53 +0900

    1011
    1011

--- 221010/index5.js
+++ 221010/index5.js
@@ -1,10 +1,10 @@
 let slides = document.querySelector(".slides");
 let slide = document.querySelectorAll(".slides li");
-currrnetIdx = 0;
-slideCount = slide.length;
+let currentIdx = 0;
+let slideCount = slide.length;
 let prevBtn = document.querySelector(".prev");
-slideWidth = 300;
-slideMargin = 30;
+let slideWidth = 300;
+let slideMargin = 30;
 let nextBtn = document.querySelector(".next");
 
 slides.style.width =
@@ -14,3 +14,22 @@ function moveSlide(num) {
   slides.style.left = -num * 330 + "px";
   currentIdx = num;
 }
+
+nextBtn.addEventListener("click", function () {
+  if (currentIdx < slideCount - 3) {
+    moveSlide(currentIdx + 1);
+    console.log(currentIdx);
+  } else {
+    moveSlide(0);
+  }
+});
+
+prevBtn.addEventListener("click", function () {
+  if (currentIdx > 0) {
+    moveSlide(currentIdx - 1);
+  } else {
+    moveSlide(slideCount - 3);
+  }
+});
+
+console.log(currentIdx);
