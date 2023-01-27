61bfa2434b31e5082bc666612f7f0ed65c239e35
pippen11 <91ssm@naver.com>
2023-01-27 22:15:28 +0900

    df

--- programers/index.js
+++ programers/index.js
@@ -2231,3 +2231,23 @@ let db = [
 // } else answer = lastprice - money;
 
 // console.log(answer);
+/////////////////////////////////
+
+// let arr1 = [
+//   [1, 2],
+//   [2, 3],
+// ];
+// let arr2 = [
+//   [3, 4],
+//   [5, 6],
+// ];
+// let sum = [];
+// let first = [];
+// let second = [];
+// for (let i = 0; i < arr1.length; i++) {
+//   console.log(arr1[i][i]);
+//   // console.log(arr1[i][i]);
+//   // console.log(arr1[i][1] + arr2[i][1]);
+// }
+// // console.log(first);
+// // console.log(second);
