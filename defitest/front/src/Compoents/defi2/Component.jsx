cfc807bad82b139df4069aa42aaacb26b5507a2a
pippen11 <111038332+pippen11@users.noreply.github.com>
2023-04-05 20:23:34 +0900

    230405

--- defitest/front/src/Compoents/defi2/Component.jsx
+++ defitest/front/src/Compoents/defi2/Component.jsx
@@ -1,10 +1,4 @@
-import { BlockVisionProvider, BvNetwork } from "blockvision.js";
-import { useState } from "react";
-
 function DefitwoCompoent() {
-  const [deFiPortfolio, setDeFiPortfolio] = useState([]);
-  const bv = new BlockVisionProvider(BvNetwork.ETH_MAINNET);
-
   return (
     <div>
       <button>defi테스트</button>
